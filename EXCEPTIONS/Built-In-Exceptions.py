79a1de3bafe2b0b9a3d7cbfb03e119448d617633
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2023-03-27 13:33:08 -0300

    ROADMAP

--- EXCEPTIONS/Built-In-Exceptions.py
+++ EXCEPTIONS/Built-In-Exceptions.py
@@ -1,9 +1,57 @@
+#Imports 
+
+import sys 
+
 #The base class for all built-in exceptions. It is not meant to be directly inherited by user-defined classes (for that, use Exception). If str() is called on an instance of this class, the representation of the argument(s) to the instance are returned, or the empty string when there were no arguments.
 try:
-    x = int(input("Type a number: "))
+    x = int(input("Type a negative number: "))
     if x < 0:
-        raise ValueError("Negative numbers are not allowed")
+        raise ValueError("Negative numbers are not allowed >:3")
 except(ValueError, BaseException) as e:
     print("Error: ",e)
     
+print('\n')
+
+#The tuple of arguments given to the exception constructor
+class MyException(Exception):
+    def __init__(self,arg1,arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2 
+        super().__init__(f"MyException was raised with args: {arg1}, {arg2}")
+        
+try: 
+    raise MyException("value", "value2")    
+except MyException as e:
+    print(f"Exception args: {e.args}")
+
+print('\n')
+
+#This method sets tb as the new traceback for the exception and returns the exception object. It was more commonly used before the exception chaining features of PEP 3134 became available.
+try:
+    1 / 0
+except ZeroDivisionError as e:
+    
+    tb = sys.exc_info()[2]
+    
+    new_exception = ValueError("Something went wrong").with_traceback(tb)
+    
+    raise new_exception
+
+#Add the string note to the exception’s notes which appear in the standard traceback after the exception string. A TypeError is raised if note is not a string.
+class CustomException(Exception):
+    def __init__(self, message):
+        self.notes = []
+        super().__init__(message)
+
+    def add_note(self, note):
+        if not isinstance(note, str):
+            raise TypeError("Note must be a string")
+        self.notes.append(note)
+
+try:
+    raise CustomException("Something went wrong!")
+except CustomException as e:
+    e.add_note("Additional information about the error")
+    raise e
+
 #
