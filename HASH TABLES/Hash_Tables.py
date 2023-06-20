96278820e2a2d3437c04bf3ba5d11c2bf0c7638e
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2023-06-20 02:50:10 -0300

    roadmap

---
+++ HASH TABLES/Hash_Tables.py
@@ -0,0 +1,8 @@
+glossary = {"BFDL": "Benevolent Dictator For Life"}
+glossary["GIL"] = "Global Interpreter Lock" #Add
+glossary["BFDL"] = "Guido wan Rossum" #Update
+
+del glossary["GIL"]
+glossary["BFDL"]
+
+print(glossary)
