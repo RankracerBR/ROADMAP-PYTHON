078f3f794aa9b1c6624126c26f22993cfac3ae8b
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2023-06-21 13:51:42 -0300

    roadmap

--- HASH TABLES/Hash_Tables.py
+++ HASH TABLES/Hash_Tables.py
@@ -1,5 +1,6 @@
 import string
-
+from collections import Counter
+from string import printable
 
 #1
 glossary = {"BFDL": "Benevolent Dictator For Life"}
@@ -54,4 +55,74 @@ class Person:
 print(hash(Person))
 
 print(hash(Person()))
+print('\n')
 
+#4
+
+def distribute(items, num_containers, hash_function=hash):
+    return Counter([hash_function(item) % num_containers for item in items])
+
+def plot(histogram):
+    for key in sorted(histogram):
+        count = histogram[key]
+        padding = (max(histogram.values()) - count) * ""
+        print(f"{key:3} {'■'}{padding}({count})")
+        
+plot(distribute(printable, num_containers=2))
+
+plot(distribute(printable, num_containers=5))
+print('\n')
+
+#5
+
+print(id("Lorem"))
+print('\n')
+
+#6
+
+def hash_function(text):
+    return sum(ord(character) for character in text) # hash collision
+
+print(hash_function("Lorem"))
+print(hash_function("Loren"))
+print(hash_function("Loner")) 
+print('\n')
+
+#7
+
+def hash_function_2(key):
+    return sum(ord(character) for character in str(key)) #dont differ string to a number
+
+print(hash_function_2("Lorem"))
+print(hash_function_2(3.14))
+print(hash_function_2(True))
+print(hash_function_2("3.14"))
+print(hash_function_2(3.14))
+print('\n')
+
+#8
+
+def hash_function_3(key):
+    return sum(ord(character) for character in repr(key))
+
+print(hash_function_3("3.14"))
+print(hash_function_3(3.14))
+print('\n')
+
+#9
+
+def hash_function_4(key):
+    return sum(
+        index * ord(character)
+        for index, character in enumerate(repr(key), start = 1)
+    )
+    
+print(hash_function_4("tity"))
+print(hash_function_4("This has a somewhat medium length."))
+print(hash_function_4("This is very long and slow!" * 1_000_00))
+print(hash_function_4("Tiny") % 100)
+print(hash_function_4("This has somewhat medium length.") % 100)
+print(hash_function_4("This is very long and slow.!" * 1_000_00) % 100) 
+print('\n')
+
+#10
