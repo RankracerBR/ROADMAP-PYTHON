4c8574521788a6b588c8b61537d7222a3063cde1
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2023-06-29 08:17:11 -0300

    roadmap

--- HASH__TABLES/.pytest_cache/Hashable_Keys.py
+++ HASH__TABLES/.pytest_cache/Hashable_Keys.py
@@ -17,20 +17,25 @@ print('\n')
 class Person:
     #__hash__ = None This will prevent hash() from working on instances of your class.
     
-    def __init__(self, name):
+    def __init__(self, name, date_of_birth, married):
         self.name = name
+        self.date_of_birth = date_of_birth
+        self.married = married
+    
+    def __hash__(self):
+        return hash(self.name)
 
     def __eq__(self, other):
         if self is other:
             return True
-
         if type(self) is not type(other):
             return False
         
-        return self.name == other.name
+        return self.name == other._fields
     
-    def __hash__(self):
-        return hash(self.name)
+    @property
+    def _fields(self):
+        return self.name, self.date_of_birth, self.married
 
 print(hash(Person("Joe")))
 
@@ -54,3 +59,4 @@ bob.name = "Bobby"
 #employess[Person("Bobby")]
 
 #employess[Person("Bob")]
+
