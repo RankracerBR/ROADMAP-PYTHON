2bbcde41d695fbce82df8686ede0f70502840f29
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2023-11-30 14:20:00 -0300

    roadmap

--- LIST COMPREHENSIONS/List_Comp.py
+++ LIST COMPREHENSIONS/List_Comp.py
@@ -17,8 +17,23 @@ def get_price(price):
 def get_weather_data():
     return random.randrange(90, 110)
 
+
+
+def get_price2(txn):
+    return txn * (1 + TAX_RATE)
+
 def get_prices_with_map():
-    return list(map(get_price, txns))
+    return list(map(get_price2, txns))
+
+def get_prices_with_comprehension():
+    return [get_price(txn) for txn in txns]
+
+def get_prices_with_loop():
+    prices = []
+    for txn in txns:
+        prices.append(get_price2(txn))
+    return prices
+
 
 #Variables
 squares = []
@@ -113,10 +128,15 @@ for row in matrix:
 print(flat)
 
 #
-print(sum(i * i for i in range(1000000000))) #Dont run this
+#print(sum(i * i for i in range(1000000000))) #Dont run this
 
 #
-print(sum(map(lambda i: i*i, range(1000000000))))
+#print(sum(map(lambda i: i*i, range(1000000000))))
 
 #
-print(timeit.timeit(get_))
+TAX_RATE = .08
+txns = [random.randrange(100) for _ in range(100000)]
+
+print(timeit.timeit(get_prices_with_map, number=100))
+print(timeit.timeit(get_prices_with_comprehension, number=100))
+print(timeit.timeit(get_prices_with_loop, number=100))
